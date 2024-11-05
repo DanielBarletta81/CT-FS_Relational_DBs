594ac50f2e51542a639f1fb2872c8331c9c87035
DanielBarletta81 <dbarcelona1481@gmail.com>
2024-11-04 23:39:44 -0500

    initial upload, db schema and ERD

---
+++ book_haven_ERD.py
@@ -0,0 +1,18 @@
+#Task 2: Entity-Relationship Diagram (ERD) Creation
+
+#Develop an ERD to visually represent the relationships between the tables in your schema.
+#Highlight key relationships such as the link between Books and Authors, 
+# and Customers and Transactions.
+#Use standard ERD notation to illustrate one-to-many and many-to-one relationships.
+
+
+
+
+
+
+
+
+
+
+#Expected Outcome: A clear and detailed ERD that accurately reflects 
+# the relationships within the "BookHaven" database.
---
+++ book_haven_db.py
@@ -0,0 +1,25 @@
+#1. Designing and Implementing a Relational Database for a Bookstore
+#Objective: The aim of this assignment is to apply the concepts of relational database design, 
+# normalization, and entity-relationship modeling to 
+# create an efficient and structured database for a bookstore management system, "BookHaven."
+
+#Problem Statement: "BookHaven" is facing challenges in managing its extensive collection
+#  of books, author information, customer data, and transaction records. 
+# The current system has issues with data redundancy, inconsistency, and inefficient
+#  data retrieval.
+
+#  Your task is to design a relational database that addresses these problems
+#  while ensuring data integrity and ease of access.
+
+#Task 1: Database Schema Design
+
+#Design a normalized database schema for "BookHaven."
+
+#Identify and create tables such as Books, Authors, Customers, and Transactions.
+
+#Define columns for each table with appropriate data types and constraints.
+
+
+#Expected Outcome: A well-structured database schema diagram or document
+#  outlining tables, columns, data types, primary keys, foreign keys, and any relevant constraints.
+
